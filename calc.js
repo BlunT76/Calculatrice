42e6b62929743a869e1997f3da1cb2dc738cb94f
philippe <huhmiel@free.fr>
2018-04-27 07:50:56 +0200

    minor changes

--- calc.js
+++ calc.js
@@ -1,20 +1,4 @@
 //globale variables
-document.getElementById('1').value = 1;
-document.getElementById('2').value = 2;
-document.getElementById('3').value = 3;
-document.getElementById('4').value = 4;
-document.getElementById('5').value = 5;
-document.getElementById('6').value = 6;
-document.getElementById('7').value = 7;
-document.getElementById('8').value = 8;
-document.getElementById('9').value = 9;
-document.getElementById('0').value = 0;
-document.getElementById('divide').value = "/";
-document.getElementById('times').value = "*";
-document.getElementById('minus').value = "-";
-document.getElementById('plus').value = "+";
-document.getElementById('equals').value = "&equals";
-document.getElementById('dot').value = ".";
 var display = document.getElementById("display");
 display.innerHTML = 0;
 var a = 0;
@@ -27,7 +11,6 @@ function reply_numbers(clicked_id) {
     if (operation == null) {
 
         if (a == 0) {
-            //alert(clicked_id);
             a = document.getElementById(clicked_id).value;
             display.innerHTML = a;
             console.log("a:", a);
@@ -38,7 +21,6 @@ function reply_numbers(clicked_id) {
         }
     } else if (operation != null) {
         if (b == 0) {
-            //alert(clicked_id);
             b = document.getElementById(clicked_id).value;
             display.innerHTML = b;
             console.log("b:", b);
@@ -54,8 +36,6 @@ function reply_numbers(clicked_id) {
 function reply_operation(clicked_id) {
     if (operation == null) {
         operation = document.getElementById(clicked_id).value;
-        //display.innerHTML = "";
-        console.log("operation:", operation);
     } 
     else if(operation != null){
         reply_calc();
@@ -80,14 +60,10 @@ function reply_calc(clicked_id) {
         case "/":
             result = a / b;
     }
-    //calcul = a + operation + b;
-    //= Number(calcul);
-    //console.log("calcul:", calcul);
-    //display.innerHTML = "";
     display.innerHTML = result;
     console.log("result:", result);
     a = result;
     b = 0;
     operation = null;
 }
-//calc();
+
