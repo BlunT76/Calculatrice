b169db00723218d89f2629e8621872c5abb27b38
philippe <huhmiel@free.fr>
2018-04-27 09:24:53 +0200

    added comments, deleted all console.log

--- calc.js
+++ calc.js
@@ -6,43 +6,44 @@ var b = 0;
 var operation = null;
 var calcul = null;
 
-
+//recupere les value des nombres cliqué et
+//les assigne sur "var a" ou "var b" en fonction de la "var operation"
 function reply_numbers(clicked_id) {
     if (operation == null) {
 
         if (a == 0) {
             a = document.getElementById(clicked_id).value;
             display.innerHTML = a;
-            console.log("a:", a);
         } else {
             a = a + document.getElementById(clicked_id).value;
             display.innerHTML = a;
-            console.log("a:", a);
         }
     } else if (operation != null) {
         if (b == 0) {
             b = document.getElementById(clicked_id).value;
             display.innerHTML = b;
-            console.log("b:", b);
         } else {
             b = b + document.getElementById(clicked_id).value;
             display.innerHTML = b;
-            console.log("b:", b);
         }
     }
 
 }
 
+//recupere les value des touches +-/* et modifie la "var operation"
 function reply_operation(clicked_id) {
     if (operation == null) {
         operation = document.getElementById(clicked_id).value;
-    } 
-    else if(operation != null){
+    } else if (operation != null) {
         reply_calc();
         operation = document.getElementById(clicked_id).value;
     }
 }
 
+//Gere la touche =, transforme var a et var b en nombres,
+//Lance le calcul et met le resultat dans "var result"
+//Affiche le resultat et remet "var b" a 0 et "var a" a "var result" pour pouvoir enchainer plusieurs calculs.
+//Remet "var operation" a la valeur null
 function reply_calc(clicked_id) {
     var result = 0;
     a = Number(a);
@@ -61,9 +62,7 @@ function reply_calc(clicked_id) {
             result = a / b;
     }
     display.innerHTML = result;
-    console.log("result:", result);
     a = result;
     b = 0;
     operation = null;
 }
-
